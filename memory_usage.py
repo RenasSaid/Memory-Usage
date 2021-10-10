d958ff3b3e911c52ec9dd563b234d2e13ec880cf
Molnet2013 <winlof@hotmail.com>
2021-10-10 20:12:48 +0200

    Update memory_usage.py with loop and methods

--- memory_usage.py
+++ memory_usage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys, os, time
 
 class Memory_Check(object):
     def __init__(self):
@@ -10,6 +10,8 @@ class Memory_Check(object):
     def main(self, memory_limit, minutes):
         self.memory_limit = memory_limit
         self.minutes = minutes
+        
+        self.run_timers()
 
         over_the_limit = self.check_memory_usage(self.memory_limit, self.minutes)
 
@@ -18,6 +20,7 @@ class Memory_Check(object):
         pass
 
     def check_memory_usage(self, memory_limit, minutes):
+        pass
         
     
     def message_user(self, over_the_limit):
@@ -26,7 +29,24 @@ class Memory_Check(object):
         else:
             print("Memory limit has not been reached.")
 
-    pass
+    def run_timers(self): #starta timers
+        interval =0
+        while(True):
+            time.sleep(1)
+            self.five_min_event()
+            interval += 1
+
+            if (interval == 12):
+                self.five_min_event()
+                self.sixty_min_event()
+                interval = 0
+        
+
+    def five_min_event(self): #event var 5min, kolla om över 50Mb ram usage 
+        print("5min")
+
+    def sixty_min_event(self): #event var 60min, kolla om över 1Gb ram usage
+        print("60min")
 
 if(__name__ == "__main__"):
     args = sys.argv[1:]
